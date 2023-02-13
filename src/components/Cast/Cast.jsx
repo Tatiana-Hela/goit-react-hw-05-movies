90913baff08a4491af330d98ebf3a6a3200c5a00
Tatiana-Hela <malya9472@gmail.com>
2023-02-13 20:42:12 +0200

    fixed cast

--- src/components/Cast/Cast.jsx
+++ src/components/Cast/Cast.jsx
@@ -41,29 +41,30 @@ const Cast = () => {
         visible={loading && true}
       />
       {error && <Error />}
-      <ul className={css.list}>
-        {actors.map(({ id, name, profile_path, character }) => (
-          <li key={id} className={css.item}>
-            <img
-              src={
-                profile_path
-                  ? 'https://image.tmdb.org/t/p/w500' + profile_path
-                  : 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png'
-              }
-              alt={name}
-              width={120}
-              height={150}
-            />
-            <p className={css.name}>{name}</p>
-            <p>Character: {character}</p>
-            {actors.length === 0 && !error && (
-              <p className={css.comment}>
-                We don't have any actors for this movie
-              </p>
-            )}
-          </li>
-        ))}
-      </ul>
+      {actors.length > 0 && !error &&  (
+        <ul className={css.list}>
+          {actors.map(({ id, name, profile_path, character }) => (
+            <li key={id} className={css.item}>
+              <img
+                src={
+                  profile_path
+                    ? 'https://image.tmdb.org/t/p/w500' + profile_path
+                    : 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png'
+                }
+                alt={name}
+                width={120}
+                height={150}
+              />
+              <p className={css.name}>{name}</p>
+              <p>Character: {character}</p>
+            </li>
+          ))}
+        </ul>
+      )}
+
+      {actors.length === 0 && !error && (
+        <p className={css.comment}>We don't have any actors for this movie</p>
+      )}
     </>
   );
 };
